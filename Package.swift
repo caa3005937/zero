fbb5288d956ab6d1bd31e8833e6013b7853ff1df
Carlos Arismendy <carismendy@viewnext.com>
2024-11-08 10:25:31 +0100

    init

--- Package.swift
+++ Package.swift
@@ -4,20 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "zero",
+    name: "SPMArtifactory",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "zero",
+            name: "SPMArtifactory",
             targets: ["zero"]),
     ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+    dependencies: [
+        .package(url: "https://github.com/danielgindi/Charts.git", .upToNextMajor(from: "5.1.0")),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "10.21.0")),
+        .package(url: "https://github.com/googlemaps/ios-maps-sdk", .upToNextMajor(from: "8.4.0")),
+        .package(url: "https://github.com/googlemaps/ios-places-sdk", .upToNextMajor(from: "8.3.0")),
+        .package(url: "https://github.com/evgenyneu/keychain-swift.git", .upToNextMajor(from: "21.0.0")),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", branch: "master"),
+        .package(url: "https://github.com/salesforce-marketingcloud/MarketingCloudSDK-iOS", .upToNextMajor(from: "8.0.3")),
+        .package(url: "https://github.com/marmelroy/PhoneNumberKit", exact: "3.7.6"),
+    ], targets: [
         .target(
-            name: "zero"),
-        .testTarget(
-            name: "zeroTests",
-            dependencies: ["zero"]),
+            name: "zero"
+            ),
+        
     ]
 )
