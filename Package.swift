0a36f31925b44502dd0cbb45a33a13e3c95d1a43
Carlos Arismendy <carismendy@viewnext.com>
2024-11-08 13:32:40 +0100

    Una dependencia

--- Package.swift
+++ Package.swift
@@ -12,17 +12,11 @@ let package = Package(
             targets: ["zero"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/danielgindi/Charts.git", .upToNextMajor(from: "5.1.0")),
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "10.21.0")),
-        .package(url: "https://github.com/googlemaps/ios-maps-sdk", .upToNextMajor(from: "8.4.0")),
-        .package(url: "https://github.com/googlemaps/ios-places-sdk", .upToNextMajor(from: "8.3.0")),
-        .package(url: "https://github.com/evgenyneu/keychain-swift.git", .upToNextMajor(from: "21.0.0")),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", branch: "master"),
-        .package(url: "https://github.com/salesforce-marketingcloud/MarketingCloudSDK-iOS", .upToNextMajor(from: "8.0.3")),
-        .package(url: "https://github.com/marmelroy/PhoneNumberKit", exact: "3.7.6"),
     ], targets: [
         .target(
-            name: "zero"
+            name: "zero",
+            packageAccess: true
             ),
         
     ]
