2031865ecc3b5160c943e36d91801384e39ae5a1
Carlos Arismendy <carismendy@viewnext.com>
2024-11-08 12:24:35 +0100

    Change name

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "SPMArtifactory",
+    name: "zero",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SPMArtifactory",
+            name: "zero",
             targets: ["zero"]),
     ],
     dependencies: [
